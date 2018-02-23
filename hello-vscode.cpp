749b3980158f6c0bb723064df0126be44dc2b84a
daniel <daniel@VMdebian820x64>
2018-02-23 16:43:38 +0800

    HelloTest

---
+++ hello-vscode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(int argc, const char * argv[]) {
+    std::cout << "hello Visual Studio Code! :)" << '\n'; 
+    std::cout << "Try Debug! :)" << '\n';
+    std::cout << "Try gdb build! :)" << '\n';
+    std::cout << "Try make build! :)" << '\n';
+    return 0;
+}
+
+// #include <stdio.h>
+// int main() {
+//     printf("hello world c! \n");
+//     return 0;
+// }
